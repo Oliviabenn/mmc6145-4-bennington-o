c18ccaf09100f702a9e8641dad237e1b4276bc05
Olivia <oliviabennington@gmail.com>
2023-03-10 18:47:42 -0500

    reducer implemented for each action

--- src/context/book/reducer.js
+++ src/context/book/reducer.js
@@ -1,8 +1,30 @@
 // TODO: import actions and implement reducer for each action//
-import{
-  
+
+import {
+  ADD_BOOK,
+  REMOVE_BOOK,
+  SEARCH_BOOKS
+} from './actions.js'
+
+
+export default function reducer(prevState {action, payload}){
+  const {favoriteBooks} = prevState
+  let newFavoriteBooks
+  switch(action) {
+    case ADD_BOOK:
+      newFavoriteBooks = [...favoriteBooks, payload]
+      saveToLocalStorage(newFavoriteBooks)
+      return {...prevState, favoriteBooks: newFavoriteBooks} 
+    case REMOVE_BOOK:
+      newFavoriteBooks = favoriteBooks.filter(book => book.id != payload)
+      saveToLocalStorage(newFavoriteBooks)
+      return {...prevState, favoriteBooks: newFavoriteBooks} 
+    case SEARCH_BOOKS:
+      return {...prevState, bookSearchResults: payload}
+    default:
+      return prevState
+  }
 }
-export default function reducer() {}
 
 // This helper function stores the favoriteBook state in localStorage as a string
 function saveToLocalStorage(favBooks) {
