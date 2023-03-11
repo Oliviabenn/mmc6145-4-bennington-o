acee7aafe3128e9f7571b98f4d5e231d120e1d9b
Olivia <oliviabennington@gmail.com>
2023-03-10 20:41:02 -0500

    formatting, reducer

--- src/context/book/reducer.js
+++ src/context/book/reducer.js
@@ -1,32 +1,25 @@
 // TODO: import actions and implement reducer for each action//
 
-import {
-  ADD_BOOK,
-  REMOVE_BOOK,
-  SEARCH_BOOKS
-} from './actions.js'
+import { ADD_BOOK, REMOVE_BOOK, SEARCH_BOOKS } from "./actions.js"
 
 
 export default function reducer(prevState, {action, payload}){
-  const {favoriteBooks} = prevState
+  const {bookSearchResults, favoriteBooks} = prevState
   let newFavoriteBooks
   switch(action) {
     case ADD_BOOK:
-      newFavoriteBooks = [...favoriteBooks, payload]
-      saveToLocalStorage(newFavoriteBooks)
-      return {...prevState, favoriteBooks: newFavoriteBooks} 
+      saveToLocalStorage([...favoriteBooks, payload])
+      return {...prevState, favoriteBooks: [...favoriteBooks, payload]} 
 
     case REMOVE_BOOK:
-      newFavoriteBooks = favoriteBooks.filter(book => book.id != payload)
-      saveToLocalStorage(newFavoriteBooks)
-      return {...prevState, favoriteBooks: newFavoriteBooks}
+      saveToLocalStorage(favoriteBooks.filter(book => book.id != payload))
+      return {...prevState, favoriteBooks: favoriteBooks.filter(book => book.id != payload)}
 
     case SEARCH_BOOKS:
       return {...prevState, bookSearchResults: payload}
     default:
       return prevState
-  }
-}
+  }}
 
 // This helper function stores the favoriteBook state in localStorage as a string
 function saveToLocalStorage(favBooks) {
